dd17e90c5108c674afdb6097bf46e2bb022de012
Ryan Perlman <ryperl@microsoft.com>
2018-03-16 16:45:49 -0700

    progress up to 17

---
+++ src/components/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends Component {
+
+    render() {
+        return <input onChange={event => console.log(event.target.value)} />;
+    }
+}
+
+export default SearchBar;
